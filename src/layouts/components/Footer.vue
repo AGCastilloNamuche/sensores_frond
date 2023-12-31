7df470a91e736e1ec905c3ebbfd6552952fb3ee7
Anthony Gian Pierre Castillo Namuche <anthony.castillo@agrovisioncorp.com>
2023-12-30 23:05:26 -0500

    subida del sistema de sensores

---
+++ src/layouts/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="h-100 d-flex align-center justify-space-between">
+    <!-- 👉 Footer: left content -->
+    <span class="d-flex align-center">
+      COPYRIGHT
+      &copy;
+      {{ new Date().getFullYear() }}
+      <a href="https://agrovisioncorp.com/" target="_blank" rel="noopener noreferrer" style="color: #45B39D;" class="ms-1">Agrovision
+        Perú SAC</a>
+    </span>
+    <!-- 👉 Footer: right content -->
+    <span class="d-md-flex gap-x-4 d-none" style="color: #45B39D;">
+      <span>Agricultura de precisión<VIcon color="#45B39D" icon="tabler-code"></VIcon></span>
+    </span>
+  </div>
+</template>
---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,23 @@
+export default [
+  // {
+  //   title: 'Agrovision',
+  //   icon: { icon: '' },
+  //   children:[
+  //     {
+  //       title: 'A9-E07C02-T04A',
+  //       icon: { icon: 'tabler-file' },
+  //       to: { name: 'index' },
+  //     }
+  //   ]
+  // },
+  {
+    title: ' Monitorización',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-heart-rate-monitor' },
+  },
+  {
+    title: 'Resumen',
+    to: { name: 'second-page' },
+    icon: { icon: 'tabler-adjustments-cog' },
+  },
+]
